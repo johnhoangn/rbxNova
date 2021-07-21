52253b9aee371670ed178bb6d3c2e45c7e1063ab
John Nguyen <johnhoangn@gmail.com>
2021-07-21 17:10:17 -0400

    begin controls

--- src/Client/Services/ShipService.lua
+++ src/Client/Services/ShipService.lua
@@ -6,10 +6,11 @@
 
 
 
-local ShipService = {}
-local EntityService, MetronomeService
+local ShipService = {Priority = 80}
+local EntityService, MetronomeService, Network, FakeCAS
 
 local UserShip, ProcessJobID
+local ControlsMaid
 
 
 -- Own ship updater job
@@ -19,20 +20,40 @@ local function ProcessUserShip(dt)
 end
 
 
--- Waits for EntityService to create our ship and saves it
+-- Receives the ship provided to us by the server
+-- If the base had not replicated yet, we return false
+--  which leads to the server to retrying 
+-- @param dt <float>
+-- @param base <Model>
+local function ReceiveShip(dt, base)
+    return ShipService:SetShip(base)
+end
+
+
+-- Grabs our ship entity from EntityService, saves it, and starts processing physics
 -- @param base <Model>
 -- @returns <boolean> success
 function ShipService:SetShip(base)
-    local ship = EntityService:WaitEntity(base)
+    if (base == nil) then
+        warn("Base nil! Not replicated yet?")
+        return false
+    end
+
+    local ship = EntityService:GetEntity(base, true)
 
     if (ship ~= nil) then
         UserShip = ship
         ProcessJobID = MetronomeService:BindToFrequency(60, ProcessUserShip)
         self.ShipCreated:Fire(base)
 
+        self.LocalPlayer.Character.PrimaryPart.Anchored = true
+        workspace.CurrentCamera.CameraSubject = base
+
+        self:EnableControls(true)
+
         return true
     else
-       return false
+        return false
     end
 end
 
@@ -47,6 +68,7 @@ end
 -- Destroys the user's ship
 function ShipService:RemoveShip()
     if (UserShip ~= nil) then
+        self:EnableControls(false)
         MetronomeService:Unbind(ProcessJobID)
         UserShip:Destroy()
         UserShip = nil
@@ -55,16 +77,75 @@ function ShipService:RemoveShip()
 end
 
 
+-- Binds or unbinds user input to the user's ship
+-- TODO: Determine input method
+-- @param state <boolean>
+function ShipService:EnableControls(state)
+    if (state) then
+        local inputStates = {
+            W = false;
+            S = false;
+            A = false;
+            D = false;
+
+            LMB = false;
+            RMB = false;
+        }
+
+        FakeCAS:BindAction("ThrottleMax", function(_, state)
+            inputStates.W = state == Enum.UserInputState.Begin
+
+            if (inputStates.W) then
+                UserShip:SetThrottle(1)
+            else
+                UserShip:SetThrottle(inputStates.S and -1 or 0)
+            end
+
+            print("W", inputStates.W)
+        end, Enum.KeyCode.W)
+
+        FakeCAS:BindAction("ThrottleMin", function(_, state)
+            inputStates.S = state == Enum.UserInputState.Begin
+
+            if (inputStates.S) then
+                UserShip:SetThrottle(-1)
+            else
+                UserShip:SetThrottle(inputStates.W and 1 or 0)
+            end
+
+            print("S", inputStates.S)
+        end, Enum.KeyCode.S)
+
+        FakeCAS:BindAction("Steer", function(_, state)
+            inputStates.LMB = state == Enum.UserInputState.Begin
+            print("Click", inputStates.LMB)
+        end, Enum.UserInputType.MouseButton1)
+
+    else
+        FakeCAS:UnbindAction("ThrottleMax")
+        FakeCAS:UnbindAction("ThrottleMin")
+        FakeCAS:UnbindAction("Steer")
+    end
+
+    self.ControlsEnabled = state
+end
+
+
 function ShipService:EngineInit()
     EntityService = self.Services.EntityService
     MetronomeService = self.Services.MetronomeService
+    Network = self.Services.Network
+    FakeCAS = self.Modules.FakeCAS
+
+    ControlsMaid = self.Classes.Maid.new()
 
     self.ShipCreated = self.Classes.Signal.new()
+    self.ControlsEnabled = false
 end
 
 
 function ShipService:EngineStart()
-
+    Network:HandleRequestType(Network.NetRequestType.ShipControl, ReceiveShip)
 end
 
 
