395c817a960690f507fbd68b64d18f69ddae8cdf
John Nguyen <johnhoangn@gmail.com>
2021-07-21 17:10:00 -0400

    math finally correct for coaxial/lateral thrust

--- src/Shared/Classes/EntityShip.lua
+++ src/Shared/Classes/EntityShip.lua
@@ -1,11 +1,20 @@
 local AssetService
 
-local BodyVelocity2 = require(script.Parent.BodyVelocity2)
-local ThrottleController = require(script.Parent.ThrottleController)
+-- RoPID.new(kP: number, kI: number, kD: number, min: number?, max: number?)
+--      :Calculate(setPoint: number, processValue: number, deltaTime: number)
+local PID = require(script.Parent.RoPID)
 local Entity = require(script.Parent.Entity)
 local EntityShip = {}
 EntityShip.__index = EntityShip
 setmetatable(EntityShip, Entity)
+
+local ABS = math.abs
+local SIGN = math.sign
+
+local COAXIAL = Vector3.new(0, 0, -1)
+local LATERAL = Vector3.new(1, 0, 0)
+local YAW = Vector3.new(0, 1, 0)
+local XZ_PLANE = Vector3.new(1, 0, 1)
 
 
 function EntityShip.new(base, initialParams)
@@ -14,11 +23,53 @@ function EntityShip.new(base, initialParams)
 	local self = Entity.new(base, initialParams)
     local asset = AssetService:GetAsset(initialParams._BaseID)
 
+    self._Root = base.PrimaryPart
     self._Asset = asset
-    self._Thruster = BodyVelocity2.fromVectorForce(base.PrimaryPart.Thruster)
-    self._Steer = base.PrimaryPart.Steer
 
-    --self.Throttle = ThrottleController.new(acceleration, deceleration, minSpeed, maxSpeed)
+    -- Grab attributes for convenience
+    self.ThrustCoaxial = base:GetAttribute("ThrustCoaxial")
+    self.ThrustLateral = base:GetAttribute("ThrustLateral")
+    self.ThrustYaw = base:GetAttribute("ThrustYaw")
+    self.SpeedFwd = base:GetAttribute("SpeedFwd")
+    self.SpeedRev = base:GetAttribute("SpeedRev")
+    self.SpeedYaw = base:GetAttribute("SpeedYaw")
+
+    self._Forces = {
+        Coaxial = base.PrimaryPart.CoaxialForce;
+        Lateral = base.PrimaryPart.LateralForce;
+        Yaw = base.PrimaryPart.YawTorque;
+    };
+
+    self._PIDs = {
+        Coaxial = PID.new(1, 0, 0, -self.ThrustCoaxial, self.ThrustCoaxial);
+        Lateral = PID.new(1, 0, 0, -self.ThrustLateral, self.ThrustLateral);
+        Yaw = PID.new(1, 0, 0, -self.SpeedYaw, self.SpeedYaw)
+    }
+
+    self._SteerDelta = 0
+
+    self._Throttle = {
+        Ratio = 0;
+        Value = 0;
+    }
+
+    -- Auto track and auto cleanup attributes
+    self:GetMaid():GiveTask(
+        base.AttributeChanged:Connect(function(attr)
+            local attrVal = base:GetAttribute(attr)
+            local thrustSubStr, b = attr:find("Thrust")
+
+            self[attr] = attrVal
+
+            if (thrustSubStr ~= nil) then
+                self._PIDs[attr:sub(b + 1)].Bounds.Min = -attrVal
+                self._PIDs[attr:sub(b + 1)].Bounds.Max = attrVal
+
+            elseif (attr:find("Speed") ~= nil) then
+                self:SetThrottle(self.Throttle.Value)
+            end
+        end)
+    )
 
 	return setmetatable(self, EntityShip)
 end
@@ -32,11 +83,55 @@ function EntityShip:PlaceAt(cf)
 end
 
 
+-- Puts the throttle handle in a position, from which the desired speed is calculated
+--  from which the PID responsible for forward thrust will determine how hard to push
+-- The lateral thrusters will always attempt to zero out any lateral slip
+-- @param ratio <float> [0, 1]
+function Entity:SetThrottle(ratio)
+    self._Throttle.Ratio = ratio
+    self._Throttle.Value = ratio > 0 and ratio * self.SpeedFwd or ratio * self.SpeedRev
+end
+
+
+-- Gives the ship a position to look at and we calculate how "in common" the angles are
+--  from which the complement implies how "different" they are. This info is enough for
+--  the PID responsible for calculating the torque necessary to turn towards the position
+-- If given nil, the delta will be set to zero, and the PID will output zero torque
+-- @param targetPosition <Vector3> == nil
+function EntityShip:SetSteer(targetPosition)
+    if (targetPosition ~= nil) then
+        self._SteerDelta = (targetPosition - self._Root.Position).Unit:Dot(self._Root.CFrame.LookVector)
+    else
+        self._SteerDelta = 0
+    end
+end
+
+
 -- Steps bodymovers
 -- @param dt <float>
 function EntityShip:UpdatePhysics(dt)
-    --self._Thruster.Velocity = Vector3.new(0, 0, -1)
-    self._Thruster:Step(dt)
+    local root = self._Root
+
+    local currentVelocity = root.AssemblyLinearVelocity * XZ_PLANE
+    local currentSpeed = currentVelocity.Magnitude
+    local desiredDirection = root.CFrame.LookVector * XZ_PLANE
+    local desiredSpeed = self._Throttle.Value
+
+    -- Figure the speed components to divide the thrust into two: coaxial (main thruster) and lateral (side thrusters)
+    -- Yielding two scalars, so the PIDs only compute their respective local axes
+    local coaxialDot = currentSpeed > 0 and desiredDirection:Dot(currentVelocity.Unit) or 0
+    local lateralDot = currentSpeed > 0 and root.CFrame.RightVector:Dot(currentVelocity.Unit) or 0
+
+    -- Calculate necessary forces
+    local coaxialOutput = self._PIDs.Coaxial:Calculate(SIGN(self._Throttle.Ratio) * desiredSpeed, currentSpeed * coaxialDot, dt)
+    local lateralOutput = self._PIDs.Lateral:Calculate(0, currentSpeed * lateralDot, dt)
+
+    -- Scale thrust up by mass to achieve target speeds at defined acceleration rates
+    self._Forces.Coaxial.Force = coaxialOutput * root.AssemblyMass * COAXIAL
+    self._Forces.Lateral.Force = lateralOutput * root.AssemblyMass * LATERAL
+
+    -- Lastly, also update the yaw controller
+    
 end
 
 
