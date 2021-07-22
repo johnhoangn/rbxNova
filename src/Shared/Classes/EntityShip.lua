86c89dc5456ffa2aa95a1df6d8ba55da5b40d58f
John Nguyen <johnhoangn@gmail.com>
2021-07-22 04:26:22 -0400

    moved attribute setting to shipservice (server)

--- src/Shared/Classes/EntityShip.lua
+++ src/Shared/Classes/EntityShip.lua
@@ -34,39 +34,19 @@ function EntityShip.new(base, initialParams)
     AssetService = AssetService or EntityShip.Services.AssetService
 
 	local self = Entity.new(base, initialParams)
+    local asset = AssetService:GetAsset(initialParams._BaseID)
 
     self._Root = base.PrimaryPart
-    self._Asset = AssetService:GetAsset(initialParams._BaseID)
+    self._Asset = asset
 
-    -- Grab attributes for convenience
-    self.ThrustCoaxial = base:GetAttribute("ThrustCoaxial")
-    self.ThrustLateral = base:GetAttribute("ThrustLateral")
-    self.ThrustYaw = math.rad(base:GetAttribute("ThrustYaw"))
-    self.SpeedFwd = base:GetAttribute("SpeedFwd")
-    self.SpeedRev = base:GetAttribute("SpeedRev")
-    self.SpeedYaw = math.rad(base:GetAttribute("SpeedYaw"))
+    self.ThrustCoaxial = asset.ShipData.ThrustCoaxial
+    self.ThrustLateral = asset.ShipData.ThrustLateral
+    self.ThrustYaw = math.rad(asset.ShipData.ThrustYaw)
+    self.SpeedFwd = asset.ShipData.SpeedFwd
+    self.SpeedRev = asset.ShipData.SpeedRev
+    self.SpeedYaw = math.rad(asset.ShipData.SpeedYaw)
 
-    self._Forces = {
-        Coaxial = base.PrimaryPart.CoaxialForce;
-        Lateral = base.PrimaryPart.LateralForce;
-        Yaw = base.PrimaryPart.YawTorque;
-    };
-
-    self._PIDs = {
-        -- Tuning PID gains is actually just an art; these are 100% trial + error
-        Coaxial = PID.new(1, 0, 0, -self.ThrustCoaxial, self.ThrustCoaxial);
-        Lateral = PID.new(1, 0, 0, -self.ThrustLateral, self.ThrustLateral);
-        Yaw = PID.new(1, 0, 0, -self.ThrustYaw, self.ThrustYaw)
-    }
-
-    self._SteerDirection = 0
-
-    self._Throttle = {
-        Ratio = 0;
-        Value = 0;
-    }
-
-    -- Auto track and auto cleanup attributes
+    -- Auto update attributes
     self:GetMaid():GiveTask(
         base.AttributeChanged:Connect(function(attr)
             local attrVal = base:GetAttribute(attr)
@@ -81,17 +61,39 @@ function EntityShip.new(base, initialParams)
 
                 self._PIDs[target].Bounds.Min = -attrVal
                 self._PIDs[target].Bounds.Max = attrVal
+                self[attr] = attrVal
 
             elseif (attr:find("Speed") ~= nil) then
                 if (attr == "SpeedYaw") then
                     attrVal = math.rad(attrVal)
                 end
+                self[attr] = attrVal
                 self:SetThrottle(self._Throttle.Value)
             end
-
-            self[attr] = attrVal
         end)
     )
+
+    -- Thrusters
+    self._Forces = {
+        Coaxial = base.PrimaryPart.CoaxialForce;
+        Lateral = base.PrimaryPart.LateralForce;
+        Yaw = base.PrimaryPart.YawTorque;
+    };
+
+    -- Thrust controllers
+    self._PIDs = {
+        -- Tuning PID gains is actually just an art; these are 100% trial + error
+        Coaxial = PID.new(1, 0, 0, -self.ThrustCoaxial, self.ThrustCoaxial);
+        Lateral = PID.new(1, 0, 0, -self.ThrustLateral, self.ThrustLateral);
+        Yaw = PID.new(1, 0, 0, -self.ThrustYaw, self.ThrustYaw)
+    }
+
+    -- PID inputs
+    self._SteerDirection = 0
+    self._Throttle = {
+        Ratio = 0;
+        Value = 0;
+    }
 
 	return setmetatable(self, EntityShip)
 end
@@ -158,8 +160,6 @@ function EntityShip:UpdatePhysics(dt)
     -- Also update yaw torque
     self._Forces.Yaw.Torque = yawOutput * root.AssemblyMass * YAW
 
-    print(currentSpeed, math.deg(root.AssemblyAngularVelocity.Y))
-
     self._DebugVelocityPart.Size = Vector3.new(.2, .2, currentSpeed)
     self._DebugVelocityPart.CFrame = CFrame.lookAt(root.Position, root.Position + currentVelocity) * CFrame.new(0, 0, -self._DebugVelocityPart.Size.Z/2)
     -- TODO: Apply thruster effectiveness modifiers (when a side or the aft is blown up or the ship is in a weight threshold)
@@ -175,6 +175,7 @@ local serverConstructor = EntityShip.new
 function EntityShip.new(...)
     local self = serverConstructor(...)
     self.Base.Hitboxes:Destroy()
+    self.Base.Hardpoints:Destroy()
     self._DebugVelocityPart = DebugPart()
     self._DebugVelocityPart.Parent = self.Base
     return self
@@ -184,18 +185,44 @@ end
 -- Renders this EntityShip
 function EntityShip:Draw()
     if (self._Model == nil) then
+        local config = self._InitialParams.Config
         local model = self._Asset.Model:Clone()
-        local parts = {}
+        local hardpoints = self._Asset.Hardpoints:Clone()
+ 
+        -- Stick the chassis on the base
+        model:SetPrimaryPartCFrame(self.Base.PrimaryPart.CFrame)
+        model.Parent = self.Base
+        self.Modules.WeldUtil:WeldParts(model.PrimaryPart, self.Base.PrimaryPart)
 
+        -- Hardpoints
+        hardpoints:SetPrimaryPartCFrame(model.PrimaryPart.CFrame)
+        self.Modules.WeldUtil:WeldParts(hardpoints.PrimaryPart, model.PrimaryPart)
+        hardpoints.Parent = model
+
+        for section, sectionData in pairs(config.Sections) do
+            local modelSection = hardpoints:FindFirstChild(section)
+
+            if (modelSection == nil) then continue end
+            for uid, attachmentData in pairs(sectionData.Attachments) do
+                if (attachmentData.Hardpoint ~= nil) then
+                    local attachModel = AssetService:GetAsset(attachmentData.BaseID, self.Base).Model:Clone()
+                    local attachPart = modelSection[attachmentData.Hardpoint]
+
+                    attachModel:SetPrimaryPartCFrame(attachPart.CFrame)
+                    self.Modules.WeldUtil:WeldParts(attachModel.PrimaryPart, attachPart)
+                    attachModel.Parent = modelSection
+                end
+            end
+        end
+
+        -- Opacity aesthetics
+        local parts = {}
         for _, part in ipairs(model:GetDescendants()) do
             if (part:IsA("BasePart") and part.Transparency < 1) then
                 parts[part] = part.Transparency
             end
         end
 
-        model:SetPrimaryPartCFrame(self.Base.PrimaryPart.CFrame)
-        model.Parent = self.Base
-        self.Modules.WeldUtil:WeldParts(model.PrimaryPart, self.Base.PrimaryPart)
         self._Model = model
         self._Parts = parts
     else
